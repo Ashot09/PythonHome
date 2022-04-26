6dcfaa26de90b25a5238e0f23cebb29ed92a0b96
Ashot09 <ashpoghosyan98@gmail.com>
2022-04-26 09:46:18 +0400

    lesson

--- lesson.py
+++ lesson.py
@@ -1,1 +1,12 @@
-print("hello")
+x = 'a,2,b,5,c,8,a,4,e,11'
+x = x.split(",")
+dict1 = {}
+for i in range(0,len(x),2):
+    key  = x[i]
+    value = int(x[i+1])
+    if key in dict1:
+        dict1[key] += value
+    else:
+        dict1[key] = value
+print(dict1)
+
